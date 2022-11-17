52917c2dbbc7c8c66437c1e9df3c9070ef4b6e4a
Tony Hammack <hammack.tony@gmail.com>
2022-11-16 22:25:45 -0600

    fix: Point to theoraclenetwork.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tonyhammack.com',
+  site: 'https://theoraclenetwork.com',
   integrations: [tailwind(), mdx(), svelte()],
   markdown: {
     shikiConfig: {
