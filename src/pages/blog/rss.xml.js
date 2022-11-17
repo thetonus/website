1607bdd4171fac6265b1548d84fd571503468f67
Tony Hammack <hammack.tony@gmail.com>
2022-11-16 21:38:37 -0600

    feat: Add rss

---
+++ src/pages/blog/rss.xml.js
@@ -0,0 +1,28 @@
+import rss from "@astrojs/rss";
+import getPostData from "../../utils/getPostData";
+
+const postImportResult = import.meta.glob("../../../content/blog/*.md", {
+  eager: true,
+});
+// Sort in descending order
+const posts = Object.values(postImportResult).sort(
+  (a, b) =>
+    new Date(b.frontmatter.publishDate).valueOf() -
+    new Date(a.frontmatter.publishDate).valueOf()
+);
+
+export const get = () =>
+  rss({
+    title: "Tony Hammack’s Blog",
+    description: "A place for my thoughts...",
+    site: import.meta.env.SITE,
+    items: posts.map((post) => {
+      const { slug } = getPostData(post);
+      return {
+        link: `posts/${slug}`, // rss module injects the SITE, only needs to work on the path
+        title: post.frontmatter.title,
+        publishDate: post.frontmatter.publishDate,
+      };
+    }),
+    customData: `<language>en-us</language>`,
+  });
