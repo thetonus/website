21de80bca50e0f6240efaacd570875519a0bf448
Tony Hammack <hammacktony@users.noreply.github.com>
2023-01-17 19:17:25 -0600

    Add open graph and update favicon (#7)
    * Add sitemaps
    * Add opengraph and update favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://theoraclenetwork.com',
-  integrations: [tailwind(), mdx(), svelte()],
+  site: "https://theoraclenetwork.com",
+  integrations: [tailwind(), mdx(), svelte(), sitemap()],
   markdown: {
     shikiConfig: {
-      theme: 'nord'
+      theme: "nord"
     },
-    rehypePlugins: [['rehype-external-links', {
-      target: '_blank'
+    rehypePlugins: [["rehype-external-links", {
+      target: "_blank"
     }]]
   }
 });
